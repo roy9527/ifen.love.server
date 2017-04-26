c9be5b0b2b64985a1f10fe3001702502a7e73ac3
kin <zhangjing@iappease.com.cn>
2017-04-26 19:17:52 +0800

    update

--- manage.py
+++ manage.py
@@ -3,10 +3,20 @@ import os
 
 from app import create_app
 
-from flask_script import Manager
+from flask_script import Manager, Server, Shell
 
 app = create_app('default')
 manager = Manager(app)
+
+def make_shell_context():
+    return dict(app=app)
+
+manager.add_command("runserver", Server(
+    use_debugger = True,
+    use_reloader = True,
+    host = '0.0.0.0')
+)
+manager.add_command('shell', Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
     manager.run()
